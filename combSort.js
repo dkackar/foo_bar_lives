9e2b9ff5345ff84a74e2570b74c04945c2df3685
Deepa Kackar <dkackar99@yahoo.com>
2016-03-14 09:15:07 -0700

    Assessment JS

--- combSort.js
+++ combSort.js
@@ -5,7 +5,8 @@ var combSort = function(arr){
   //In bubble sort the distance between 2 elements being compared
   //from each other is 1, but in combo sort it can be more than 1.
  
-  // The initial gap will be arr size / shrink
+  // The initial gap in loop will be arr size / shrink
+  
   var gap =  arr.length;  
 
  // as recommended shrink size
@@ -14,7 +15,7 @@ var combSort = function(arr){
   var swapped = true;
   
   while (gap != 1 || swapped == true) {
-    // gap is the 
+    // assign new gap 
     gap = Math.floor(gap / shrink);
   
     if (gap < 1) {
@@ -25,12 +26,13 @@ var combSort = function(arr){
     var i = 0;
     swapped = false;
         
-    //a single "comb" over the input list
+    //a single "comb" over the the array
     while((i + gap)  < arr.length) {
       
+      //compare the elements for the current gap 
       if (arr[i] > arr[i+gap]) {
         
-        //below swaps the variable
+        //below swaps the variable if the first element is > 
         var temp = arr[i];
         arr[i] = arr[i+gap];
         arr[i+gap] = temp; 
